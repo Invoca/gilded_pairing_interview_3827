f38cd6022dcce1c303ed3e5273740a0c5442fff7
Sean Corbett <stcorbett@gmail.com>
2023-07-07 22:24:18 -0500

    Integration level test of an inventory of items

---
+++ ruby/spec/inventory_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+describe 'Inventory' do
+  context "#update_price" do
+    let(:item) { double('item') }
+    let(:inventory) { Inventory.new([item]) }
+
+    it "calls update_item_for_day on each item in the inventory" do
+      expect(item).to receive(:update_item_for_day)
+      inventory.update_price
+    end
+  end
+end
---
+++ ruby/spec/store_integration_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+describe 'Gilded Rose store' do
+  context 'A mixed inventory of items' do
+    let(:items) {
+      [
+        {
+          item: Item.new('Normal Item', 10, 20, 'Apple'),
+          becomes: { sell_by: 9, price: 19 }
+        },
+        {
+          item: Item.new('Fine Art', 10, 20, 'Mona Lisa'),
+          becomes: { sell_by: 9, price: 21 }
+        },
+        {
+          item: Item.new('Concert Tickets', 9, 20, 'Nickelback'),
+          becomes: { sell_by: 8, price: 22 }
+        },
+        {
+          item: Item.new('Gold Coins', 10, 80, 'Spanish Doubloons'),
+          becomes: { sell_by: 10, price: 80 }
+        },
+        {
+          item: Item.new('Materials', 10, 20, 'Copper Wire'),
+          becomes: { sell_by: 9, price: 19 }
+        },
+      ]
+    }
+    let(:inventory) { Inventory.new(items.map { |item| item[:item] }) }
+    before{ inventory.update_price }
+
+    it 'updated prices and sell_by for each item' do
+      items.each do |test_case|
+        item = test_case[:item]
+
+        expect(item.sell_by).to eq(test_case[:becomes][:sell_by])
+        expect(item.price).to eq(test_case[:becomes][:price])
+      end
+    end
+  end
+end
